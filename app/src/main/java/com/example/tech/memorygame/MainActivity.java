7803b093ac672297f15d3e79c6aacd09e13aa7cf
Michael Noseworthy <michael.noseworthy@georgebrown.ca>
2018-03-16 09:11:30 -0400

    Game logic added

--- app/src/main/java/com/example/tech/memorygame/MainActivity.java
+++ app/src/main/java/com/example/tech/memorygame/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.tech.memorygame;
-
+/**
+ * Card Memory Game, Madalyn Kendrick and Michael Noseworthy, March 16, 2018, Card game where player selects a card and tries to match that card with another selected card.
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatDrawableManager;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +17,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+}
+
+//Button setup for the cards.
+public class MemoryGameButton extends android.support.v7.widget.AppCompatButton {
+
+    protected int row;
+    protected int column;
+    protected int frontDrawableId;
+
+    protected boolean isFlipped = false;
+    protected boolean isMatched = false;
+    protected Drawable front;
+    protected Drawable back;
+
+    int[] cardsArray = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                        21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+                        41,42,43,44,45,46,47,48,49,50,51,52};
+
+
+    public MemoryGameButton(Context context, int r, int c, int frontImageDrawableId)
+    {
+        super(context);
+
+        row = r;
+        column = c;
+        frontDrawableId = frontImageDrawableId;
+
+        front = AppCompatDrawableManager.get().getDrawable(context, frontImageDrawableId);
+        back = AppCompatDrawableManager.get().getDrawable(context, R.drawable.button_question_mark);
+
+        setBackground(back);
+
+        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r), GridLayout.spec(c));
+
+        tempParams.width = (int) getResources().getDisplayMetrice().density * 50;
+        tempParams.height = (int) getResources().getDisplayMetrice().density * 50;
+        setLayoutParams(tempParams);
+
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+
+    public int getFrontDrawableId() {
+        return frontDrawableId;
+    }
+
+    public void flip()
+    {
+        if(isMatched)
+            return;
+
+        if(isFlipped)
+        {
+            setBackground(back);
+            isFlipped = false;
+        }
+
+        else
+        {
+            setBackground(front);
+            isFlipped = true;
+        }
+    }
+
+    private void frontOfCardsResources(){
+
+        for(int i = 0; i < 52; i++)
+        {
+            int card[]
+            image101 = R.drawable.ic_image101;
+        }
+
+    }
+
 }
